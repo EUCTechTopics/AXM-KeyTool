1e8c25ee144810d8fc52ef3dafae46e3950cfab1
Mathijs de Ruiter <33999622+EUCTechTopics@users.noreply.github.com>
2025-07-01 14:16:49 +0200

    Initial Commit

---
+++ AXM KeyTool/AXM_KeyToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  AXM_KeyToolApp.swift
+//  AXM KeyTool
+//
+//  Created by Mathijs de Ruiter on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AXM_KeyToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
